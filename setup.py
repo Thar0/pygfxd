c56a889fc1b9ab6b4932c00119da8f34a560d776
Ethan Roseman <ethteck@gmail.com>
2022-05-20 22:37:27 +0900

    wheel, test, deploy

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+#  copied from https://stackoverflow.com/questions/4529555/building-a-ctypes-based-c-library-with-distutils
+from setuptools import setup, Extension
+from distutils.command.build_ext import build_ext as build_ext_orig
+
+
+class CTypesExtension(Extension):
+    pass
+
+
+class build_ext(build_ext_orig):
+    def build_extension(self, ext):
+        self._ctypes = isinstance(ext, CTypesExtension)
+        return super().build_extension(ext)
+
+    def get_export_symbols(self, ext):
+        if self._ctypes:
+            return ext.export_symbols
+        return super().get_export_symbols(ext)
+
+    def get_ext_filename(self, ext_name):
+        if self._ctypes:
+            return ext_name + '.so'
+        return super().get_ext_filename(ext_name)
+
+
+setup(
+    name="pygfxd",
+    version="1.0.0",
+    py_modules = ["pygfxd"],
+    ext_modules=[
+        CTypesExtension(
+            "libgfxd",
+            [
+                "libgfxd/gfxd.c",
+                "libgfxd/uc_f3d.c",
+                "libgfxd/uc_f3db.c",
+                "libgfxd/uc_f3dex.c",
+                "libgfxd/uc_f3dex2.c",
+                "libgfxd/uc_f3dexb.c",
+                "libgfxd/uc.c",
+            ],
+            include_dirs=["libgfxd"],
+        ),
+    ],
+    cmdclass={'build_ext': build_ext},
+)

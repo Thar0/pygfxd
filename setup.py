605e30e908d3e7b3369b4b26ed07aee49d4dc0b7
Ethan Roseman <ethteck@gmail.com>
2022-05-20 13:37:00 -0400

    Add libgfxd .c/.h files to MANIFEST (#4)

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pygfxd",
-    version="1.0.0",
+    version="1.0.1",
     author="Tharo",
     description="Python bindings for libgfxd",
     long_description=long_description,

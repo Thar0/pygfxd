9ee2149fe9a875d4d90362cd8786726fb4097e8c
Tharo <17233964+Thar0@users.noreply.github.com>
2022-12-21 21:24:12 +0000

    Revert "Build wheels on Windows (#6)"
    This reverts commit 91f1bf15d43ddfc376a1de545e10fe6e26bad5b8.

--- setup.py
+++ setup.py
@@ -48,11 +48,6 @@ setup(
                 "libgfxd/uc.c",
             ],
             include_dirs=["libgfxd"],
-            extra_compile_args = [
-                "-std=c11",
-                "-Wall",
-                "-g",
-            ],
         ),
     ],
     cmdclass={'build_ext': build_ext},
